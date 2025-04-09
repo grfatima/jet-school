d3b55d7ea91535eec7c4b6eb1073c966da96d973
grfatime <kurbanovhafatime@gmail.com>
2025-04-09 15:26:54 +0400

    dropdown

--- js/app.js
+++ js/app.js
@@ -5,14 +5,24 @@ const langs = ["AZ", "EN", "RU"];
 let activeLang = langs[0];
 let deActiveLangs = langs.filter((l) => l !== activeLang);
 
-let rightArrow = `<i class="fa-solid fa-arrow-right arrow"></i>`;
-let downArrow = `<i class="fa-solid fa-arrow-down"></i>`;
+const showLangs = () => {
+  let rightArrow = `<i class="fa-solid fa-arrow-right arrow"></i>`;
+  let downArrow = `<i class="fa-solid fa-arrow-down"></i>`;
 
-activeLangBox.innerHTML = activeLang + rightArrow;
-deActiveLangs.forEach((lang) => {
-  deActiveLangBox.innerHTML += `<li>${lang}</li>`;
-});
+  activeLangBox.innerHTML = activeLang + rightArrow;
+  deActiveLangs.forEach((lang) => {
+    deActiveLangBox.innerHTML += `<li onclick = setActiveLang('${lang}')>${lang}</li>`;
+  });
+};
+showLangs();
 
 activeLangBox.onclick = () => {
   deActiveLangBox.classList.toggle("show");
 };
+
+function setActiveLang(lang) {
+  deActiveLangBox.innerText = "";
+  activeLang = lang;
+  deActiveLangs = langs.filter((l) => l !== activeLang);
+  showLangs();
+}
