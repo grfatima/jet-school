267c4496a00ec2c1967a7142e9044c354f1b30de
grfatime <kurbanovhafatime@gmail.com>
2025-04-09 14:05:50 +0400

    dropdown menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-const headLangBox = document.querySelector("#headLangBox");
+const activeLangBox = document.querySelector("#headLangBox");
+const deActiveLangBox = document.querySelector("#deActiveLang");
 
 const langs = ["AZ", "EN", "RU"];
 let activeLang = langs[0];
+let deActiveLangs = langs.filter((l) => l !== activeLang);
 
-let rightIcon = `<i class="fa-solid fa-arrow-right arrow"></i>`;
-headLangBox.innerHTML = activeLang + rightIcon;
+let rightArrow = `<i class="fa-solid fa-arrow-right arrow"></i>`;
+let downArrow = `<i class="fa-solid fa-arrow-down"></i>`;
+
+activeLangBox.innerHTML = activeLang + rightArrow;
+deActiveLangs.forEach((lang) => {
+  deActiveLangBox.innerHTML += `<li>${lang}</li>`;
+});
+
+activeLangBox.onclick = () => {
+  deActiveLangBox.classList.toggle("show");
+};
